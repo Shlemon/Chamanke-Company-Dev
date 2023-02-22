34526e08e5d1d85539b69bfb3949f30fc6ecbef4
Ninos Dinkha <ninos.dinkha97@gmail.com>
2023-02-23 00:11:55 +0300

    added img gallery

---
+++ public/scripts/projectsPage.js
@@ -0,0 +1,88 @@
+// figure out next and prv btns
+// const imgContainer = document.querySelector('.image-container');
+const popupImgContainer = document.querySelector('.popup-image');
+const popupImg = document.querySelector('.popup-image img');
+const popupImgX = document.querySelector('.popup-image span');
+const images = Array.from(document.querySelectorAll('.image-container img'));
+
+const nextBtn = document.querySelector('.nextBtn');
+const prevBtn = document.querySelector('.previousBtn');
+
+
+images.forEach((img, index) => {
+  img.addEventListener('click', () => {
+    popupImgContainer.style.display = 'block';
+    popupImg.src = img.getAttribute('src');
+    console.log(index);
+
+    nextBtn.addEventListener('click', () => {
+      index++;
+      if (index > images.length - 1) {
+        index = 0;
+      }
+      popupImg.src = images[index].src;
+    });
+
+    prevBtn.addEventListener('click', () => {
+      index--;
+      if (index < 0) {
+        index = images.length - 1;
+      }
+      popupImg.src = images[index].src;
+    });
+
+    window.addEventListener('keydown', (e) => {
+      if (e.code === 'ArrowRight') {
+        index++;
+        if (index > images.length - 1) {
+          index = 0;
+        }
+        popupImg.src = images[index].src;
+      } else if (e.code === 'ArrowLeft') {
+        index--;
+        if (index < 0) {
+          index = images.length - 1;
+        }
+        popupImg.src = images[index].src;
+      }
+      // console.log(e.code)
+      // console.log(e.key)
+    });
+  });
+});
+
+
+popupImgContainer.addEventListener('click', (e) => {
+  console.log(e.target);
+  if (e.target === popupImgX) {
+    popupImgContainer.style.display = 'none';
+  } else if (e.target === popupImgContainer) {
+    popupImgContainer.style.display = 'none';
+  }
+});
+
+
+
+
+
+
+
+// Testing multiple events
+
+// ['click', 'keydown'].forEach((event) => {
+//   nextBtn.addEventListener(event, () => {
+//     index++;
+//     if (index > images.length - 1) {
+//       index = 0;
+//     }
+//     popupImg.src = images[index].src;
+//   });
+
+//   prevBtn.addEventListener(event, () => {
+//     index++;
+//     if (index > images.length - 1) {
+//       index = 0;
+//     }
+//     popupImg.src = images[index].src;
+//   });
+// });
